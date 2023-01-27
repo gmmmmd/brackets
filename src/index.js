0c760e84a3c40d66327009a7ba82bdeebaefff8e
gmmmmd <zoopark86@yandex.ru>
2023-01-27 20:13:11 +0400

    feat: add complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 === 0) {
+    for(let i = 0; i < str.length * 40; i++) {
+      bracketsConfig.map(item => {
+          const bracket = item.join('');
+          if (str.includes(bracket)) str = str.replace(bracket,'');
+          return bracket;
+      });
+    }
+    return str.length ? false : true;
+  } else {
+    return false;
+  }
 }
